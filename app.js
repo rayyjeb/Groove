9d5568b97fa313ead68dadc769bf9ca27fdfb107
rayyjeb <rajab.shoukath@kalvium.community>
2023-08-26 00:38:15 +0530

    groove

---
+++ app.js
@@ -0,0 +1,6 @@
+var buybtn = document.getElementById("BUYNOW")
+var text = document.getElementById("thanks-text")
+buybtn.onclick=()=>{
+    buybtn.style.display='none'
+    text.style.display='block'
+}
